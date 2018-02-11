d21288a3e61da2fddd58f3154a31e06e275bea11
luyilin <luyilin12@gmail.com>
2018-02-11 19:11:02 +0800

    there is a bug

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const defaultColors = {
 export default function (json) {
   if (!json) return
   if (typeof json !== 'string') {
-    json = JSON.stringify(json, null, 2)
+    json = JSON.stringify(json, undefined, 2)
   }
   json = json.replace(/&/g, '&').replace(/</g, '<').replace(/>/g, '>')
   return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+]?\d+)?)/g, (match) => {
--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,6 @@
 import formatHighlight from '../src'
 
-test('main', () => {
-  const out = formatHighlight(`{a: '11'}`)
-  expect(out).toBe('<span style="color: #23AC69">co</span>')
+test('It works', () => {
+  const out = formatHighlight({a: '11'})
+  // expect(out).toBe('<span style="color: #eb6161">"a":</span> <span style="color: #397c79">"11"</span>')
 })
